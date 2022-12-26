413dfc68964b7249f789a015ba6140eeb2633b0b
Aminev Ural <aminevural51@gmail.com>
2022-12-26 17:09:33 +0300

    Deleted a comment

--- UtilityFees.Data/Entities/FullMeasurement.cs
+++ UtilityFees.Data/Entities/FullMeasurement.cs
@@ -8,10 +8,7 @@ public class FullMeasurement
     public double DailyElectricityAmount { get; set; }
     public double NightElectricityAmount { get; set; }
     public double HotWaterAmount { get; set; }
-
     public double ColdWaterAmount { get; set; }
-
-    //public int MeasurementYear { get; set; }
     public int MeasurementMonth { get; set; }
     public User? User { get; set; }
     public string? UserId { get; set; }
